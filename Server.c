be8b3b2d06a81151c389367166a010cda3984ea6
Marc Willms <mwillms@htwsaar.de>
2017-06-11 11:38:45 +0200

    Create Server.c
    Ergänzungen von Doxygen-Kommentaren im bereits intakten Programmabaluf

--- Server.c
+++ Server.c
@@ -1,6 +1,13 @@
+
 /*
-    C socket server example, handles multiple clients using threads
-*/
+ * @Server.c
+ * @author  Gruppe  Wirth
+ * @version 1.0
+ *
+ * @section DESCRIPTION
+ *
+ * Hauptprogramm zum Sicherstellen eienr Serverseitigen Kommunikation.
+ */
 
 #include<stdio.h>
 #include<string.h>    //strlen
@@ -13,45 +20,57 @@
 #define MAX_WORDS 5
 
 //the thread function
+
 void *connection_handler(void *);
 
+/*
+ * Hauptprogramm
+ * @param int socket_desc Socketnummer
+ * @param int client_socket Socketnummer des Clients
+ * @param int c Rechenwert
+ * @param int new_sock pointer auf neuen socket
+ * @param sockadr_in_server Server speichert hier Clientadresse
+*/
 int main(int argc, char *argv[]) {
-    int socket_desc, client_sock, c, *new_sock;
+    int socket_desc;
+    int client_sock;
+    int c;
+    int *new_sock;
     struct sockaddr_in server, client;
 
-    //Create socket
+    //Socket erstellen, Fehlermeldung wenn nicht
     socket_desc = socket(AF_INET, SOCK_STREAM, 0);
     if (socket_desc == -1) {
         printf("Could not create socket");
     }
-    puts("Socket created");
+    puts("Socket erstellt");
 
-    //Prepare the sockaddr_in structure
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons(8888);
+    //Strukturvorbereitung: sockaddr_in 
+    server.sin_family = AF_INET; //Adressfamilie
+    server.sin_addr.s_addr = INADDR_ANY;//egal welche Eingangsadresse
+    server.sin_port = htons(8888);//Port 8888
 
-    //Bind
+    //Verbinden, Fehlermeldung wenn nicht möglich
     if (bind(socket_desc, (struct sockaddr *) &server, sizeof(server)) < 0) {
         //print the error message
         perror("bind failed. Error");
         return 1;
     }
-    puts("bind done");
+    puts("Verbindung erfolgreich hergestellt");
 
-    //Listen
+    //Lauschen - warten auf eingehende Verbindungen
     listen(socket_desc, 3);
-
-    //Accept and incoming connection
-    puts("Waiting for incoming connections...");
+    puts("Warten auf eingehende Verbindungen...");
     c = sizeof(struct sockaddr_in);
 
 
     /*Accept and incoming connection
     puts("Waiting for incoming connections...");
     c = sizeof(struct sockaddr_in);*/
+    
+    //Verbindungshandler
     while ((client_sock = accept(socket_desc, (struct sockaddr *) &client, (socklen_t * ) & c))) {
-        puts("Connection accepted");
+        puts("Verbindung hergestellt\n");
 
         pthread_t sniffer_thread;
         new_sock = malloc(1);
@@ -64,7 +83,7 @@ int main(int argc, char *argv[]) {
 
         //Now join the thread , so that we dont terminate before the thread
         //pthread_join( sniffer_thread , NULL);
-        puts("Handler assigned");
+        puts("Handler zugewiesen");
     }
 
     if (client_sock < 0) {
@@ -74,10 +93,14 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
-
 /*
- * This will handle connection for each client
- * */
+ * Funktion zum haendeln von Verbindungen
+ * @param sock casten des Socket_desc -integer zu einem Pointer, Socketnummer
+ * @param message Nachrichten zur Eingabensteuerung
+ * @param int read_size
+ * @param char *message, client_message[2000]
+ * 
+ */
 void *connection_handler(void *socket_desc) {
     //Get the socket descriptor
     int sock = *(int *) socket_desc;
@@ -91,11 +114,11 @@ void *connection_handler(void *socket_desc) {
     int i = 0;
     int count = 0;
 
-    //Send some messages to the client
-    message = "Greetings! I am your connection handler\n";
+    //Nachrichtenuebertragungen, Begrueßung und Eingabeparameter abfragen
+    message = "Hallo ich bin dein Verbindungshaendler\n";
     write(sock, message, strlen(message));
 
-    message = "What Files are you asking for, and how many Bytes shall be given? Split the arguments with a comma.\n";
+    message = "Welche Dateien werden benötigt, wie viele Bytes sollen gelesen werden? Bitte die eingaben durch \",\" Trennen.\n";
     write(sock, message, strlen(message));
 
     //Receive a message from client
@@ -132,15 +155,15 @@ void *connection_handler(void *socket_desc) {
             }
         }*/
     }
-
+    //Verbindungsabbau
     if (read_size == 0) {
-        puts("Client disconnected");
+        puts("Verbindung zum Client getrennt\n");
         fflush(stdout);
     } else if (read_size == -1) {
         perror("recv failed");
     }
 
-    //Free the socket pointer
+    //freigeben des Socket-Pointers
     free(socket_desc);
 
     return 0;
