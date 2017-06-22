d67989fc0d9af4ff9d9522f64528f53c9f11e71f
Marc Willms <mwillms@htwsaar.de>
2017-06-22 15:47:11 +0200

    Create Dude_Imp.java

---
+++ Dude_Imp.java
@@ -0,0 +1,16 @@
+package java.io;
+
+public class FileReader extends InputStreamReader {
+
+    public FileReader(String string) throws FileNotFoundException {
+        // compiled code
+    }
+
+    public FileReader(File file) throws FileNotFoundException {
+        // compiled code
+    }
+
+    public FileReader(FileDescriptor fd) {
+        // compiled code
+    }
+}
