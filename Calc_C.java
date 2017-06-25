fdf9d78b3e717ba649539cf61b7dcf0d78c0a6cf
Marc Willms <mwillms@htwsaar.de>
2017-06-25 07:23:51 +0200

    Update Calc_C.java
    Noch ein Fehler in Zeile 33: er findet leseNamen und leseZeilen nicht. Rest ist ausgemerzt und sollte fertig sein und funzen ....

--- Calc_C.java
+++ Calc_C.java
@@ -3,12 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package calc_Client;
+package Calc_Client;
 
-import calc_Inter.Calc_Inter;
-import static calc_Server.Calc_S.standardFile;
+import Calc_Inter.Calc_Inter;
+import static Calc_Server.Calc_S.standardFile;
 //import static calc_Server.Calc_S.standardString;
-import java.io.BufferedReader;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -19,23 +18,29 @@ import java.rmi.registry.Registry;
  * @author marc
  */
 public class Calc_C {
-    public static void main (String[]args){
+    public static String namenSuchFile = "Namensliste_C.txt";
+
+    public static void main (String[]args) throws RemoteException, NotBoundException{
+
         try{
-            Registry creg=LocateRegistry.getRegistry("127.0.0.1",1051);
+            
+            Registry creg=LocateRegistry.getRegistry("127.0.0.1",1033);
             Calc_Inter di=(Calc_Inter)creg.lookup("calc");
             System.out.println("Verbindung hergestellt...\n");
+            String ziel = di.getString(standardFile);
+            System.out.println(ziel+"-Ziel");
+            System.out.println(namenSuchFile+"-Start");
+            vergleicheNamen(leseNamen(namenSuchFile),leseZeilen(ziel));
+        }catch (RemoteException | NotBoundException ex){
+                ex.printStackTrace();
+        }
             //System.out.println("1+1="+di.add(1,1));
             //System.out.println("1-1="+di.sub(1,1));
             //System.out.println("1*1="+di.mul(1,1));
             //System.out.println("1/1="+di.div(1,0));
-            //String tmp;
-            //tmp = di.getString(standardString);
-            BufferedReader br = null;
-            br = getFilet(standardFile);
+            //String tmp="";5
             //System.out.println(tmp+" Cl");
-        }catch (RemoteException | NotBoundException ex){
-                ex.printStackTrace();
-        }
     }
+
 }
     
